e61b25780b826ace12e576e1e62daadbc58b49c4
Tharindu Lakshan <rsachi0713@gmail.com>
2026-07-18 15:01:50 +0530

    pupdate 20

---
+++ .open-next/assets/_next/static/chunks/app/admin/page-0cf946e52f838b1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[137,152,366,520,526,578,623,698,758,770],{2949:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8500,23))}},e=>{e.O(0,[500,441,794,358],()=>e(e.s=2949)),_N_E=e.O()}]);
